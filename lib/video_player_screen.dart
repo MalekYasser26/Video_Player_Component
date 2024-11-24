4dd39ec9ad4bd43633a806d0ec00a0837e181934
Malek <malekyasser10@gmail.com>
2024-11-24 15:02:32 +0200

    full screen option finished with a button to go out of it and into it

--- lib/video_player_screen.dart
+++ lib/video_player_screen.dart
@@ -15,7 +15,7 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   late VideoPlayerController _controller;
   late Future<void> _initializeVideoPlayerFuture;
   bool _showControls = false;
-
+  bool fullScreen = false;
   @override
   void initState() {
     super.initState();
@@ -58,10 +58,7 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
       _resetControlVisibilityTimer();
     }
   }
-
-
   Timer? _controlVisibilityTimer;
-
   void _resetControlVisibilityTimer() {
     _controlVisibilityTimer?.cancel(); // Cancel any existing timer
     _controlVisibilityTimer = Timer(const Duration(seconds: 4), () {
@@ -79,166 +76,192 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     return Scaffold(
       body: GestureDetector(
         onTap: _toggleControls,
-        child: Center(
-          child: AspectRatio(
-            aspectRatio: _controller.value.aspectRatio,
-            child: Stack(
-              children: [
-                Center(
-                  child: FutureBuilder(
-                    future: _initializeVideoPlayerFuture,
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.done) {
-                        return VideoPlayer(_controller);
-                      } else {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                    },
-                  ),
-                ),
-                if (_showControls)
+        child: RotatedBox(
+          quarterTurns: fullScreen?1:4,
+          child: Center(
+            child: AspectRatio(
+              aspectRatio: _controller.value.aspectRatio+0.4,
+              child: Stack(
+                children: [
                   Center(
-                    child: Column(
-                      children: [
-                        Expanded(
-                          child: Container(
-                            color: Colors.grey.withOpacity(0.2),
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  IconButton(
-                                    onPressed: () {
-                                      _skipBackward();
-                                      print("Skipped backward");
-                                      _resetControlVisibilityTimer();
-                                    },
-                                    icon: const Icon(
-                                      Icons.replay_10,
-                                      size: 50,
+                    child: FutureBuilder(
+                      future: _initializeVideoPlayerFuture,
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          return VideoPlayer(_controller);
+                        } else {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                      },
+                    ),
+                  ),
+                  if (_showControls)
+                    Center(
+                      child: Column(
+                        children: [
+                          Expanded(
+                            flex: fullScreen ?13 : 1,
+                            child: Container(
+                              color: Colors.grey.withOpacity(0.2),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {
+                                        _skipBackward();
+                                        _resetControlVisibilityTimer();
+                                      },
+                                      icon: const Icon(
+                                        Icons.replay_10,
+                                        size: 50,
+                                        color: Colors.white,
+                                      ),
                                     ),
-                                  ),
-                                  IconButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        if (_controller.value.isPlaying) {
-                                          _controller.pause();
-                                          print("Paused");
-                                        } else {
-                                          _controller.play();
-                                          print("Playing");
-                                        }
-                                      });
-                                      _resetControlVisibilityTimer();
-                                    },
-                                    icon: Icon(
-                                      _controller.value.isPlaying
-                                          ? Icons.pause
-                                          : Icons.play_arrow,
-                                      size: 50,
+                                    IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          if (_controller.value.isPlaying) {
+                                            _controller.pause();
+                                            print("Paused");
+                                          } else {
+                                            _controller.play();
+                                            print("Playing");
+                                          }
+                                        });
+                                        _resetControlVisibilityTimer();
+                                      },
+                                      icon: Icon(
+                                        _controller.value.isPlaying
+                                            ? Icons.pause
+                                            : Icons.play_arrow,
+                                        size: 50,
+                                        color: Colors.white,
+                                      ),
                                     ),
-                                  ),
-                                  IconButton(
-                                    onPressed: () {
-                                      _skipForward();
-                                      print("Skipped forward");
-                                      _resetControlVisibilityTimer();
-                                    },
-                                    icon: const Icon(
-                                      Icons.forward_10,
-                                      size: 50,
+                                    IconButton(
+                                      onPressed: () {
+                                        _skipForward();
+                                        print("Skipped forward");
+                                        _resetControlVisibilityTimer();
+                                      },
+                                      icon: const Icon(
+                                        Icons.forward_10,
+                                        size: 50,
+                                        color: Colors.white,
+                                      ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        VideoProgressIndicator(
+                          Expanded(
+                            flex: fullScreen ? 1 : 0,
+                            child: Container(
+                              color: Colors.grey.withOpacity(0.2),
+                              child: VideoProgressIndicator(
+                                _controller,
+                                padding: fullScreen ?const EdgeInsets.symmetric(vertical: 10) : EdgeInsets.zero,
+                                allowScrubbing: true,
+                                colors: const VideoProgressColors(
+                                  playedColor: Colors.red,
+                                  bufferedColor: Colors.grey,
+                                  backgroundColor: Colors.black,
+                                ),
+
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  else
+                    Center(
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Expanded(
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      _toggleControls();
+                                    },
+                                    onDoubleTap: () {
+                                      _skipBackward();
+                                      _toggleControls();
+                                      Future.delayed(const Duration(seconds: 2)).whenComplete(() => _toggleControls(),);
+                                    },
+                                  ),
+                                ),
+                                Expanded(
+                                  child: GestureDetector(onTap: () {
+                                    print("here controls shown ");
+                                    _toggleControls();
+
+                                  },),
+                                ),
+                                Expanded(
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      _toggleControls();
+                                    //  Future.delayed(const Duration(milliseconds: 3000)).whenComplete(() => _toggleControls(),);
+                                    },
+                                    onDoubleTap: () {
+                                      _skipForward();
+                                      _toggleControls();
+                                  //    Future.delayed(const Duration(seconds: 2)).whenComplete(() => _toggleControls(),);
+                                      print("here skip hidden ");
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          !fullScreen ?VideoProgressIndicator(
                           _controller,
+                          padding: fullScreen ?const EdgeInsets.symmetric(vertical: 10) : EdgeInsets.zero,
                           allowScrubbing: true,
                           colors: const VideoProgressColors(
                             playedColor: Colors.red,
                             bufferedColor: Colors.grey,
                             backgroundColor: Colors.black,
                           ),
-                        ),
-                      ],
-                    ),
-                  )
-                else
-                  Center(
-                    child: Column(
-                      children: [
-                        Expanded(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Expanded(
-                                child: GestureDetector(
-                                  onTap: () {
-                                    _toggleControls();
-                                  },
-                                  onDoubleTap: () {
-                                    _skipBackward();
-                                    _toggleControls();
-                                    Future.delayed(const Duration(seconds: 2)).whenComplete(() => _toggleControls(),);
-                                  },
-                                ),
-                              ),
-                              Expanded(
-                                child: GestureDetector(onTap: () {
-                                  print("here controls shown ");
-                                  _toggleControls();
-                                  // Future.delayed(const Duration(milliseconds: 4200), () {
-                                  //   _toggleControls();
-                                  //   print("here controls hidden ");
-                                  // });
-                                },),
-                              ),
-                              Expanded(
-                                child: GestureDetector(
-                                  onTap: () {
-                                    _toggleControls();
-                                  //  Future.delayed(const Duration(milliseconds: 3000)).whenComplete(() => _toggleControls(),);
-                                  },
-                                  onDoubleTap: () {
-                                    _skipForward();
-                                    _toggleControls();
-                                //    Future.delayed(const Duration(seconds: 2)).whenComplete(() => _toggleControls(),);
-                                    print("here skip hidden ");
-                                  },
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        VideoProgressIndicator(
-                          _controller,
-                          allowScrubbing: true,
-                          colors: const VideoProgressColors(
-                            playedColor: Colors.red,
-                            bufferedColor: Colors.grey,
-                            backgroundColor: Colors.black,
-                          ),
-                        ),
+
+                                                      ) : const SizedBox.shrink()
 
 
-                      ],
+
+                        ],
+                      ),
                     ),
+                  Positioned(
+                    bottom: 5,
+                    right: 0,
+                    child: IconButton(onPressed: () {
+                      fullScreen=!fullScreen;
+                      setState(() {
+                      });
+                    }, icon: const Icon(Icons.fullscreen,size: 30,color: Colors.white,)),
                   ),
-                Positioned(
-                  bottom: 0,
-                  right: 0,
-                  child: IconButton(onPressed: () {
-                    print("fullscreen");
-                  }, icon: Icon(Icons.fullscreen,size: 30,color: Colors.white,)),
-                )
+                  fullScreen &&_showControls ? Positioned(
+                    top: 10,
+                    left: 15,
+                    child: IconButton(onPressed: () {
+                      fullScreen = false ;
+                      setState(() {
+                      });
+                    }, icon: Icon(Icons.keyboard_arrow_down_sharp,size: 50,color: Colors.white,)),
+                  ):SizedBox.shrink()
 
-              ],
+
+                ],
+              ),
             ),
           ),
         ),
